e7ae5a82d806fc31f9578788da8e564aff5c68e6
paulgenesisbacuna@gmail.com <paulgenesisbacuna@gmail.com>
2019-09-06 15:53:28 +0800

    edit header

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
-<!doctype html>
+<?php  
+  include "DB.MySQLPDO.WBMonitoring.php"; 
+  $machine_id = $_GET['mc_id'];
+  $mc_data = dbGetMachinebyID($machine_id);
+  $machine_name = $mc_data[0]["MachineName"];
+?>
+
+<!DOCTYPE html>
 <html lang="en">
   <head>
     
@@ -7,22 +14,20 @@
 
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="index.css">
+    <link rel="stylesheet" type="text/css" href="pql_css/home.css">
     <title>Amertron</title>
   </head>
   <body class="bg">
 
 
-<!-- header -->
+  <!-- header -->
+  <div class="header">
+      <img style="float:left; margin-top: .4vw;" src="images/Amertron Logo.png" class="ai-logo">
+      <h2 style="float: left; font-family: Arial, sans-serif; color: white; font-size: 1.5vw; margin-left: 10px; margin-top: 1.2vw">Amertron Incorporated</h2>
+      <!-- <h2 id="time" style="float: right; font-family: Arial, sans-serif; color: white; font-size: 1vw; margin-top:1.2vw;  margin-right: 10px;"></h2> -->
+      <span onchange="UpdateCurrentDateTime()" id="current_datetime" style="float: right; font-family: Arial, sans-serif; color: white; font-size: 1vw; margin-top:1.2vw;  margin-right: 10px;"></span>
+  </div>
   <div class="container-fluid my_container">
-    <div class="row my_row1">
-        <div class="col-md-12 my_col">
-         <img src="image/amertronlogo.png" class="logo">
-         <h3 class="amertrontext">
-           Amertron Incorporated
-         </h3>
-         <h2 id="time" style="float: right; font-family: Arial, sans-serif; color: white; font-size: 1vw; margin-top:3vw;  margin-right: 10px;"></h2>
-        </div>
-    </div>
 
     <!-- side navigation -->
     <div class="row  my_row2">
@@ -66,7 +71,7 @@
            <div class="row   my_row">
              <div class="col-lg-2   my_col d-block d-sm-none d-md-block"  >
          <img src="image/machine.png"  class="machinepicture">
-         <h3 class="textbutton">WB#30</h3>
+         <h3 class="textbutton"><?php echo $machine_name;?></h3>
         </div>
         
          <div class="col-lg-4     my_col" >
@@ -148,16 +153,43 @@
 
 
 <script type="text/javascript">
-  var timestamp = '<?=time();?>';
-  function updateTime(){
-    $('#time').html(Date());
-    timestamp++;
+  window.setInterval(function(){UpdateCurrentDateTime()}, 1000);
+  UpdateCurrentDateTime();
+
+  function UpdateCurrentDateTime() {
+    var daysOfWeek = new Array('SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT');
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = PadZero(date.getMonth()+1);
+    var week = daysOfWeek[date.getDay()];
+    var day = PadZero(date.getDate());
+    var hours = PadZero(date.getHours());
+    var days = PadZero(date.getDay()); 
+    var minutes = PadZero(date.getMinutes());
+    var seconds = PadZero(date.getSeconds());
+    
+    var strDateTime = '(<b>'+week+'</b>) ' + year + '-' + month + '-' + day + ' ' + hours + ':' + minutes + ':' + seconds;
+    document.getElementById('current_datetime').innerHTML =  strDateTime;
   }
-  $(function(){
-    setInterval(updateTime, 1000);
-  });
+
+  function PadZero(num) {
+    return (num < 10 ? '0' + num : num);
+  }
 </script>
-  </body>
+</body>
 </html>
 
+<?php  
+  function dbGetMachinebyID($machine_id) {
+    $sql = "SELECT * FROM 13wip.`mc_master` WHERE Machine_ID = '$machine_id' ORDER BY machine_id";
+
+    $conn = dbConnect();
+    $stmt = $conn->prepare($sql);
+    $stmt->execute(array());
+    $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $conn=null;
+
+    return $data;
+  }
+?>
 
